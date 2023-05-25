9b8e2246f30bdaa15a900870897059a27a0cebc0
DAMDAVE <121645121+DAMDAVE@users.noreply.github.com>
2023-05-25 21:12:18 +0100

    inventors

--- src/Components/Movies/Movie.js
+++ src/Components/Movies/Movie.js
@@ -9,54 +9,82 @@ function Movies (props) {
   
   return (
     <div className="moviesc">
+
         <div className="allMovies">
+
           <div className="movies">
+
              <div className='topman'>
-              <p className='trend'>Trending</p>
-              <span className='right'>
-                <span className='view'>View all</span><span className='nbsp'>&nbsp;&nbsp; |&nbsp;&nbsp;</span>
-                <button className='scrollButtons'><img src={left_arrow} alt='scrollers'/></button>&nbsp;&nbsp;&nbsp;
-                <button className='scrollButtons'><img src={right_arrow} alt='scrollers'/></button>
-              </span>
+                 <p className='trend'>Trending</p>
+                 <span className='right'>
+                    <span className='view'>View all</span><span className='nbsp'>&nbsp;&nbsp; |&nbsp;&nbsp;</span>
+
+                    <button className='scrollButtons'><img src={left_arrow} alt='scrollers'/></button>&nbsp;&nbsp;&nbsp;
+
+                    <button className='scrollButtons'><img src={right_arrow} alt='scrollers'/></button>
+                 </span>
             </div>
-          <div className='movie'>  
-          <div className='movie-container'>
-           {data.results.map(
-             (movies, index) =>(
-             <span key={index} style=
-             {
-               {
-                backgroundImage: `url(https://image.tmdb.org/t/p/w500/${movies.poster_path})`
-              }
-             }  className='post' onClick={ () => onMovieClick(index)}alt='posters' >
-             <button className='buttn' >
-               <img src={plus} alt='' />
-             </button>
-             </span>
-             )
-            )
-          }
 
-          </div>
-          <div className='binge'>
-           {data.results.map(
-             (movies, index) =>(
-             <span key={index} style=
-             {
-               {
-                backgroundImage: `url(https://image.tmdb.org/t/p/w500/${movies.poster_path})`
-              }
-             }  className='post' onClick={ () => onMovieClick(index)}alt='posters' >
-             <button className='buttn' >
-               <img src={plus} alt='' />
-             </button>
-             </span>
-             )
-            )
-          }
+           <div className='movie'>
 
-          </div>
-          </div>
+              <div className='movie-container'>
+                    {data.results.map(
+                        (movies, index) =>(
+                         <span key={index} style=
+                            {{
+                              backgroundImage: `url(https://image.tmdb.org/t/p/w500/${movies.poster_path})`
+                            }}  
+                            className='post' 
+                            onClick={ () => onMovieClick(index)}
+                            alt='posters' >
+
+                             <button className='buttn' >
+                             <img src={plus} alt='' />
+                             </button>
+                          </span>
+                        )
+                      )
+                    }   
+              </div>
+
+            </div>
+
+            <div className='topman'>
+                 <p className='trend'>Binge-worthy series</p>
+                 <span className='right'>
+                    <span className='view'>View all</span><span className='nbsp'>&nbsp;&nbsp; |&nbsp;&nbsp;</span>
+
+                    <button className='scrollButtons'><img src={left_arrow} alt='scrollers'/></button>&nbsp;&nbsp;&nbsp;
+
+                    <button className='scrollButtons'><img src={right_arrow} alt='scrollers'/></button>
+                 </span>
+            </div>
+
+                 <div className='binge-container'>
+                       {data.results.map(
+                           (movies, index) =>(
+                            <div key={index}  className='bigger-box' onClick={ () => onMovieClick(index)}>
+                            <div 
+                               className='image-box' >
+                              <img 
+                                src= {`https://image.tmdb.org/t/p/w500/${movies.poster_path}`}
+                                alt='' 
+                                className='binge' />
+
+                             </div> 
+                             <div className='text-container'> 
+                             <div className='text-box'>
+                               <p>{movies.title} || <span>{movies.release_date}</span></p>
+                               <p className='overs'>{movies.overview}</p>
+                               <button></button>
+                             </div>
+                             </div>
+                             </div>
+                           )
+                         )
+                       }   
+                 </div>
+
           </div>
         </div>
     </div>
